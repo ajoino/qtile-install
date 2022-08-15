feee50717136ecfe0c587e5479aca54d62993692
ajoino <jacob.nilsson@ltu.se>
2022-08-15 16:48:00 +0200

    Added script to add required repositories.

---
+++ add-repositories.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo add-apt-repository -S deb http://deb.volian.org/volian/ scar main
+sudo add-apt-repository -P ppa:deafmute/interception
+sudo add-apt-repository -P ppa:aslatter/ppa
