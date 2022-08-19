68eeca46a5a6e9a1d695e8268016d9d5d73aa35f
ajoino <jacob.nilsson@ltu.se>
2022-08-19 23:24:02 +0200

    Added qtile requirements to the install applications file.

--- scripts/install_applications.sh
+++ scripts/install_applications.sh
@@ -1,4 +1,16 @@
 sudo nala install \
-	"libyaml-cpp0.6"
-	interception-tools
+	"libyaml-cpp0.6" \
+	interception-tools \
+	wlroots
+
+python -m pip install \
+	cffi \
+	xcffib
+
+python -m pip install \
+    cairocffi \
+	dbus-next \
+	pywayland \
+	pywlroots \
+	xkbcommon
 
